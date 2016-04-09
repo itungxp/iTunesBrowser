affd9e41042d31241b34c04989726912e234289a
Tung <nguyenthanhtung1211@gmail.com>
2016-04-09 10:46:29 +0800

    filter by kind, update pagination

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module('iTuneBrowserApp', ['ngRoute', 'ui.bootstrap'])
 
-.config(function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
         .when('/access_token=:accessToken', {
             template: '',
@@ -17,68 +17,101 @@ angular.module('iTuneBrowserApp', ['ngRoute', 'ui.bootstrap'])
                     params[key] = value;
                     $rootScope.accesstoken = params;
                 }
-                $location.path("/about");
+                $location.path('/about');
             }
         }).when('/', {
             controller: function($location) {
-                $location.path("/browse/u2");
+                $location.path('/browse/u2');
             },
             templateUrl: 'page/browser.html'
         }).when('/browse/:search', {
             controller: 'BrowserController',
             templateUrl: 'page/browser.html'
         });
+
+    // Register the HTTP INTERCEPTORS
+    $httpProvider.interceptors.push(function($q, $window, $rootScope) {
+        var loadingScreen = jQuery('<div class="app-overlay"><span></span></div>').appendTo(jQuery('body')).hide();
+        return {
+            'request': function(config) {
+                loadingScreen.show();
+                return config || $q.when(config);
+            },
+            'response': function(response) {
+                loadingScreen.hide();
+                return response || $q.when(response);
+            },
+            'responseError': function(rejection) {
+                loadingScreen.hide();
+                alert('Failed to fetch data from iTunes');
+                return $q.reject(rejection);
+            }
+        };
+    });
 })
 
 .controller('LoginCtrl', function($scope) {
     $scope.login = function() {
-        var client_id = "your client id";
-        var scope = "email";
-        var redirect_uri = "index.html";
-        var response_type = "token";
-        var url = "https://accounts.google.com/o/oauth2/auth?scope=" + scope + "&client_id=" + client_id + "&redirect_uri=" + redirect_uri +
-            "&response_type=" + response_type;
+        var client_id = 'your client id';
+        var scope = 'email';
+        var redirect_uri = 'index.html';
+        var response_type = 'token';
+        var url = 'https://accounts.google.com/o/oauth2/auth?scope=' + scope + '&client_id=' + client_id + '&redirect_uri=' + redirect_uri +
+            '&response_type=' + response_type;
         window.open(url, '_blank');
     };
 })
-.controller('BrowserController', function($scope, $location, $http, $routeParams) {
+.controller('BrowserController', function($scope, $location, $routeParams, filterFilter, SearchService) {
     $scope.init = function() {
         $scope.medias = [];
         $scope.itemsPerPage = 24;
         $scope.page = 1;
         $scope.maxSize = 5;
-        $scope.types = [{label: 'Music', kind: 'song'}, {label: 'Video', kind: 'music-video'}];
+        $scope.kind = 'musicTrack';
+        $scope.types = [{label: 'All', kind: 'musicTrack'}, {label: 'Music', kind: 'song'}, {label: 'Video', kind: 'musicVideo'}];
+        $scope.search();
+    }
 
+    $scope.search = function(){
         if ($routeParams.search) {
-            $http.jsonp("https://itunes.apple.com/search", {
+             SearchService.search($routeParams.search, $scope.kind,
+                function(data, status, headers, config) {
+                    $scope.medias = data.results;
+                    $scope.totalItems = $scope.medias.length;
+                }
+             );
+        }
+    }
+
+    $scope.filterChange = function(){
+        $scope.totalItems = filterFilter($scope.medias, $scope.filter).length;
+        console.log($scope.totalItems);
+    }
+
+    $scope.play = function(){}
+    $scope.buy = function(track){
+        window.open(track.trackViewUrl, '_blank');
+    }
+})
+
+.service('SearchService', function($http){
+    this.search = function(term, kind, callback){
+        if (term) {
+            $http.jsonp('https://itunes.apple.com/search', {
                 params: {
-                    "limit": 50,
-                    "callback": "JSON_CALLBACK",
-                    "term": $routeParams.search
+                    'entity': kind,
+                    'callback': 'JSON_CALLBACK',
+                    'term': term
                 },
                 paramsSerializer: function(param) {
                     return param;
                 }
-            }).success(function(data, status, headers, config) {
-                $scope.medias = data.results;
-            }).error(function(data, status, headers, config) {
-                alert('Failed to fetch data from iTunes');
-            });
-        }
-
-        $scope.play = function(){}
-        $scope.buy = function(track){
-            window.open(track.trackViewUrl, '_blank');
+            })
+            .success(callback);
         }
     }
 })
 
-.filter('startFrom', function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    }
-})
 .filter('textLimit', function() {
     return function(input, limit) {
         if(input.length <= limit) return input
